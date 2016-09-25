6827420152c7caf2b0376d26f0cd1d855e777982
sbalakr2 <sbalakr2@ncsu.edu>
2016-09-25 19:45:13 -0400

    create command

--- OODD_v3/db/migrate/20160923002251_create_rooms.rb
+++ OODD_v3/db/migrate/20160923002251_create_rooms.rb
@@ -1,12 +1,12 @@
 class CreateRooms < ActiveRecord::Migration[5.0]
   def change
-    #create_table :rooms do |t|
-      #t.integer :room_id
-      #t.string :building
-      #t.string :room_size
+    create_table :rooms do |t|
+      t.integer :room_id
+      t.string :building
+      t.string :room_size
 
-      #t.timestamps
-    #end
-     rename_column(:rooms, :room_id, :room_number)
+      t.timestamps
+    end
+    # rename_column(:rooms, :room_id, :room_number)
   end
 end
