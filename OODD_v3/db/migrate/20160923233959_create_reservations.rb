b4cf850c0764832a463886f5bdc1c24ea8ec21ba
sbalakr2 <sbalakr2@ncsu.edu>
2016-09-25 19:47:42 -0400

    create command

--- OODD_v3/db/migrate/20160923233959_create_reservations.rb
+++ OODD_v3/db/migrate/20160923233959_create_reservations.rb
@@ -1,15 +1,15 @@
 class CreateReservations < ActiveRecord::Migration[5.0]
   def change
-    #create_table :reservations do |t|
-      #t.integer :room_id
-      #t.integer :member_id
-      #t.date :date
-      #t.datetime :start_time
-      #t.datetime :end_time
+    create_table :reservations do |t|
+      t.integer :room_id
+      t.integer :member_id
+      t.date :date
+      t.datetime :start_time
+      t.datetime :end_time
 
-      #t.timestamps
-     change_column(:reservation, :start_time, :Time)
-     change_column(:reservation, :end_time, :Time)
+      t.timestamps
+     #change_column(:reservation, :start_time, :Time)
+     #change_column(:reservation, :end_time, :Time)
     end
   end
 end
